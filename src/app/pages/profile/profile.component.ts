97091ee9deed065674a20dc76238e46d1cf2d9d7
oscardiaz28 <diazvargasod@gmail.com>
2024-08-14 05:21:21 -0500

    add componentes for admin dashboard

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+
+interface User{
+  id: string,
+  username: string,
+  nombre: string,
+  apellido : string,
+  email : string,
+  telefono : number,
+  perfil : string,
+  roles: String[]
+}
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  user! : User;
+  
+  constructor(private loginService : LoginService){}
+
+  ngOnInit(): void {
+    this.user = this.loginService.getUser();
+  }
 
 }
